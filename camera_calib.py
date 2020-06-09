5f8c98e0f83e815360bc34dca3bfcfb5beb33b2c
Nick <nicholas_m0rris@hotmail.com>
2020-06-09 13:08:25 +1000

    Too lazy to write a proper description... Depth calibration for stereo cameras...

--- camera_calib.py
+++ camera_calib.py
@@ -19,11 +19,14 @@ class Calibration:
         self.right_imgpoints = []  # 2d points in image plane.
         self.left_images = glob.glob('support\\left_checkerboard\\*.jpg')
         self.right_images = glob.glob('support\\right_checkerboard\\*.jpg')
+        # Distance between the camera (mm)
+        self.base = 1800
 
         self.r_mtx, self.r_dist, self.r_tvecs, self.r_rvecs = self.right_calibrate()
         self.l_mtx, self.l_dist, self.l_tvecs, self.l_rvecs = self.left_calibrate()
 
         self.camera_model = self.stereo_calibrate()
+        self.depth_map()
 
         print('Camera model', self.camera_model)
 
@@ -129,6 +132,26 @@ class Calibration:
             self.r_dist, self.dimensions,
             criteria=stereocalib_criteria)
 
+        (l_rect, r_rect, l_proj, r_proj,
+         dispartityToDepthMap, l_ROI, r_ROI) = cv2.stereoRectify(
+            l_mtx, l_dist,
+            r_mtx, r_dist,
+            self.dimensions, R, T,
+            None, None, None, None, None,
+            cv2.CALIB_ZERO_DISPARITY)
+
+        leftMapX, leftMapY = cv2.initUndistortRectifyMap(
+            l_mtx, l_dist, l_rect,
+            l_proj, self.dimensions, cv2.CV_32FC1)
+        rightMapX, rightMapY = cv2.initUndistortRectifyMap(
+            r_mtx, r_dist, r_rect,
+            r_proj, self.dimensions, cv2.CV_32FC1)
+
+        # Save calibration to file
+        np.savez_compressed("support\\calib_results\\calibration.npz", imageSize=self.dimensions,
+                            leftMapX=leftMapX, leftMapY=leftMapY, leftROI=l_ROI,
+                            rightMapX=rightMapX, rightMapY=rightMapY, rightROI=r_ROI)
+
         camera_model = dict([('l_mtx', l_mtx), ('r_mtx', r_mtx), ('dist1', l_dist),
                              ('dist2', r_dist), ('rvecs1', self.l_rvecs),
                              ('rvecs2', self.r_rvecs), ('R', R), ('T', T),
@@ -138,9 +161,83 @@ class Calibration:
         print("Success")
         return camera_model
 
+    def depth_map(self):
+        """
+        Create a depth map of the stereo cameras
+
+            The camera matrix is of the following form:
+
+            f_x  s    c_x
+            0    f_y  c_y
+            0    0    1
+            where f_x is the camera focal length in the x axis in pixels
+
+        f_y is the camera focal length in the y axis in pixels
+
+        s is a skew parameter (normally not used)
+
+        c_x is the optical center in x
+
+        c_y is the optical center in y
+
+        disparity=x−x′=Bf/Z
+        x and x′ are the distance between points in image plane corresponding
+        to the scene point 3D and their camera center. B is the distance between
+        two cameras (which we know) and f is the focal length of camera (already known)
+        """
+
+        calibration = np.load("support\\calib_results\\calibration.npz")
+        imageSize = tuple(calibration["imageSize"])
+        leftMapX = calibration["leftMapX"]
+        leftMapY = calibration["leftMapY"]
+        leftROI = tuple(calibration["leftROI"])
+        rightMapX = calibration["rightMapX"]
+        rightMapY = calibration["rightMapY"]
+        rightROI = tuple(calibration["rightROI"])
+
+        stereoMatcher = cv2.StereoBM_create()
+
+        left_frame = cv2.imread("support\\left_checkerboard\\image.jpg")
+        cv2.imshow("left", left_frame)
+        cv2.waitKey(1000)
+        right_frame = cv2.imread("support\\right_checkerboard\\image.jpg")
+        fixedLeft = cv2.remap(left_frame, leftMapX, leftMapY, cv2.INTER_LINEAR)
+        fixedRight = cv2.remap(right_frame, rightMapX, rightMapY, cv2.INTER_LINEAR)
+
+        grayLeft = cv2.cvtColor(fixedLeft, cv2.COLOR_BGR2GRAY)
+        grayRight = cv2.cvtColor(fixedRight, cv2.COLOR_BGR2GRAY)
+        depth = stereoMatcher.compute(grayLeft, grayRight)
+
+        real_depth = (self.base * self.camera_model["l_mtx"][1][1]) / (depth[0][0] + 1)
+        print("REAL DEPTH", real_depth)
+        # Set parameters from albert's settings
+        stereoMatcher.setMinDisparity(4)
+        stereoMatcher.setNumDisparities(128)
+        stereoMatcher.setBlockSize(21)
+        stereoMatcher.setSpeckleRange(16)
+        stereoMatcher.setSpeckleWindowSize(45)
+
+        DEPTH_VISUALIZATION_SCALE = 2048
+        print("DEPTH", type(depth))
+        print("length", np.shape(depth))
+        np.savetxt('support\\calib_results\\depth.txt', depth, fmt='%d')
+        cv2.imshow('depth', depth / DEPTH_VISUALIZATION_SCALE)
+        cv2.imwrite("support\\calib_results\\depth.png", depth)
+        cv2.waitKey(1000)
+        cv2.destroyAllWindows()
+
+    def print_calibration(self):
+        data = np.load('support\\calib_results\\calibration.npz')
+        lst = data.files
+        for item in lst:
+            print(item)
+            print(data[item])
+
 
 def main(argv=None):
     calibrate = Calibration()
+    # calibrate.print_calibration()
+
 
 
 if __name__ == '__main__':
